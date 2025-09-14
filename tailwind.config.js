71c331596d3b7d0e206e20abd01580da69e2065a
ComfortCircleCare <info@ComfortCircleCare.co.uk>
2025-09-14 08:37:38 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#1e40af",
+        accent: "#FF7A59",
+      },
+    },
+  },
+  plugins: [],
+}
